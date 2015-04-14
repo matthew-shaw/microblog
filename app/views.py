2bd8700294fe509c5807586f32665fdd90806520
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2015-04-14 16:27:00 +0100

    bootstrapping design

--- app/views.py
+++ app/views.py
@@ -26,7 +26,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for OpenID="%s", remember_me=%s' %
-              (form.openid.data, str(form.remember_me.data)))
+              (form.openid.data, str(form.remember_me.data)), 'success')
         return redirect('/index')
     return render_template('login.html',
                            title='Sign In',
